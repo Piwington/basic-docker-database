82ff940c49ab399278d091d7afd928c5f5dc2cf8
David Lees <david@fides.me.uk>
2021-06-30 20:56:54 +0100

    Very basic insert function created.

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
 #Basic Dockerfile Template. Temp Comment Out Unused.
 #TODO Delete Any Still Commented After Completion.
-#COPY requirements.txt ./
-#RUN pip install --no-cache-dir -r requirements.txt
-
 #COPY . .
 
 #CMD [ "python", "./your-daemon-or-script.py" ]
---
+++ python/crud_functions.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+
+def postgres_connection():
+    connection = psycopg2.connect(
+        database="postgres",
+        user="user",
+        password="password"
+    )
+    return connection
+
+
+def postgres_insert(row):
+    connection = None
+    cursor = None
+    try:
+        connection = postgres_connection()
+        cursor = connection.cursor()
+        cursor.execute(
+            "INSERT INTO system_users (forename, surname, email, phone_number) VALUES (%s, %s, %s, %s)",
+            (row("forename"), row("surname"), row("email"), row("phone_number"))
+        )
+        connection.commit()
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
